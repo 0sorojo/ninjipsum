2d1fde2560a4e542ec16c9b0878b715c6c372bab
0sorojo <redrootproductions@gmail.com>
2020-10-20 20:37:34 -0400

    added basic function plus refined naruto ninjipsum

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { narutoText } from './data';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+
+    setText(narutoText.slice(0, amount));
+  };
+
   return (
     <>
-      <h1>i work</h1>
+      <section>
+        <h1>i work</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='amount'>paragraphs:</label>
+          <input
+            type='number'
+            name='amount'
+            id='amount'
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button type='submit'>generarate</button>
+        </form>
+        <article>
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
     </>
   );
 }
